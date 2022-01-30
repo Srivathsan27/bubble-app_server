a371f9a1987b2f8909fbe2795275c62c13266d49
Srivathsan <srivathsanbalaji2002@gmail.com>
2022-01-30 13:38:10 +0530

    same

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -3,8 +3,8 @@ import { Cookie } from "express-session";
 
 export const setCookie = (req: Request, id: number) => {
   req.session.userId = id;
+  req.session.cookie = new Cookie();
   req.session.cookie.sameSite = "none";
   req.session.cookie.secure = true;
   req.session.cookie.maxAge = 1000 * 60 * 60 * 24 * 365 * 1;
-  req.session.cookie = new Cookie();
 };
